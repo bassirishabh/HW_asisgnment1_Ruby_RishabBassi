b068e3d745464af4d7a09aeb6ef013a646eae630
Rishabh Bassi <bassirishabh@gmail.com>
2022-09-09 20:07:38 -0500

    Assignment1_Ruby_rishabh

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,34 +3,50 @@
 # Part 1
 
 def sum(arr)
-  # YOUR CODE HERE
+	arr.sum
 end
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  #Using sum method and max will choose 2 elem
+  arr.max(2).sum
 end
 
 def sum_to_n?(arr, number)
-  # YOUR CODE HERE
+  #comparing sum with two elem from combination method
+  arr.combination(2).select { |sel_num| sel_num.sum == number }.any?
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant?(string)
-  # YOUR CODE HERE
+  # regex means it is consonant 
+  /^[^aeiou\W]/i.match(string) == nil ? false : true
 end
 
 def binary_multiple_of_4?(string)
-  # YOUR CODE HERE
+  return true if string == "0"
+	/^[10]*00$/.match(string) != nil
 end
 
 # Part 3
 
 # Object representing a book
 class BookInStock
-  # YOUR CODE HERE
+  attr_writer :isbn,:price
+	attr_reader :isbn,:price
+
+	def initialize(isbn, price)
+		raise ArgumentError, 
+			"Illegal argument" if isbn.empty?  or price <= 0
+		@isbn = isbn
+		@price = price
+	end
+
+	def price_as_string
+		sprintf("$%2.2f", @price)
+	end
 end
